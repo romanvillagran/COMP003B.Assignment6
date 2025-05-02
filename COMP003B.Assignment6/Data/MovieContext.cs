217f67ff21826ef5a218421703c5e8765137f1f0
romanvillagran <romanvillagran386@gmail.com>
2025-05-02 00:06:54 -0700

    added my own json and also added the dbset to context

--- COMP003B.Assignment6/Data/MovieContext.cs
+++ COMP003B.Assignment6/Data/MovieContext.cs
@@ -1,6 +1,14 @@
-﻿namespace COMP003B.Assignment6.Data
+﻿using COMP003B.Assignment6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.Assignment6.Data
 {
-    public class MovieContext
+    public class MovieContext : DbContext 
     {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
     }
 }
+
