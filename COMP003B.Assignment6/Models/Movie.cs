c618046ecc0d7330e367e59f4411e1d308d77078
romanvillagran <romanvillagran386@gmail.com>
2025-05-01 21:33:43 -0700

    added models for id title director and year for movie

---
+++ COMP003B.Assignment6/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.Assignment6.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        [Range(1895, 2025)]
+        public int Year { get; set; }
+    }
+}
