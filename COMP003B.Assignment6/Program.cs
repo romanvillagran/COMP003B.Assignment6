eec2da61ac41189ee19bf95f8372f7ee04cdea50
romanvillagran <romanvillagran386@gmail.com>
2025-05-02 13:22:59 -0700

    added the configuration to program.cs

--- COMP003B.Assignment6/Program.cs
+++ COMP003B.Assignment6/Program.cs
@@ -1,3 +1,6 @@
+using COMP003B.Assignment6.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace COMP003B.Assignment6
 {
     public class Program
@@ -8,6 +11,9 @@ namespace COMP003B.Assignment6
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer("Name=ConnectionStrings:MovieContext"));
 
             var app = builder.Build();
 
