a18a8ee6e8b06702ed4d3029ec50d165e8820b74
romanvillagran <romanvillagran386@gmail.com>
2025-05-02 13:37:04 -0700

    adding scaffolding for movie and review

--- COMP003B.Assignment6/Migrations/20250502203011_AddLanguageToMovie.Designer.cs
+++ COMP003B.Assignment6/Migrations/20250502203011_AddLanguageToMovie.Designer.cs
@@ -3,6 +3,7 @@ using COMP003B.Assignment6.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace COMP003B.Assignment6.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20250502203011_AddLanguageToMovie")]
+    partial class AddLanguageToMovie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
